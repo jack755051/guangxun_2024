d401b10171502c1f26f2b831b521021476015732
charlie <jack755051@gmail.com>
2024-07-07 21:27:59 +0800

    contiue build login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LoginPageType } from '../../share/types/common.enum';
+import { CommonService } from '../../share/services/common.service';
+import { register } from 'module';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,22 @@ import { LoginPageType } from '../../share/types/common.enum';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('accountInput') accountInput!: ElementRef<HTMLInputElement>;
   isPasswordHide!: boolean;
   isForgetAccountAndPassword!: boolean;
 
   LoginPageType: any;
   loginPageType!: LoginPageType;
   buttonText: string = '';
+  loginPageHint!: { [key: string]: string[] };
 
-  constructor() {
+  constructor(private commonService: CommonService) {
     this.LoginPageType = LoginPageType;
+    this.loginPageHint = {
+      login: ["Doesn't have an account?", 'Sign up for free'],
+      register: ['Already have an account?', 'Go to Sign Up'],
+      forget: ['Forget account or password?', 'Contnet us'],
+    };
   }
 
   ngOnInit(): void {
@@ -26,8 +35,17 @@ export class LoginComponent implements OnInit {
     this.initPage();
   }
 
+  //  頁面加載完成
+  ngAfterViewInit() {
+    this.focus();
+  }
+
   initPage(): void {
     this.buttonText = LoginPageType.LOGIN.toLowerCase();
+  }
+
+  focus(): void {
+    this.accountInput.nativeElement.focus();
   }
 
   /**
@@ -40,6 +58,7 @@ export class LoginComponent implements OnInit {
         : LoginPageType.LOGIN;
 
     this.buttonText = this.loginPageType.toLowerCase();
+    this.focus();
 
     console.log('loginPageType', this.loginPageType);
   }
